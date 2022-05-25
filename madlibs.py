c04708e7ae65ba79613679c39174dacfb3797b14
Syed Salman Haider <36808172+salmanhaider93@users.noreply.github.com>
2022-05-25 16:07:49 -0700

    Add files via upload

---
+++ madlibs.py
@@ -0,0 +1,16 @@
+#string concatentiaon
+youtuber = "mrbeates " #some string variable
+
+# a few ways
+#print("subscribe to " + youtuber)
+#print("subscribe to {}".format(youtuber) )
+#print(f"subscribe to {youtuber}")
+
+adj = input("Adjective: ")
+verb1 = input("Verb: ")
+verb2 = input("Verb: ")
+famous_person = input("Famous person: ")
+
+madlib = f"Computer programming is so {adj}! It makes me so excisted all the time because \ I love to {verb1}. stay hydrated and {verb2} like you are {famous_person}!"
+
+print(madlib)
